f50b01f7bf1c1215ad7a292167a0cb1192862a46
JamesPagani <jaimeagalvezv@gmail.com>
2019-12-12 19:48:58 +0000

    Fixed PEP8 style error: Multiple imports per line

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4, sys
+    import hidden_4
+    import sys
     names = dir(hidden_4)
     for n in names:
         if n[0] != "_" and n[1] != "_":
