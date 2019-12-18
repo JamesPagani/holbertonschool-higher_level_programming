b36df525de000d21fb343e3d367896a19b5a381c
JamesPagani <jaimeagalvezv@gmail.com>
2019-12-18 00:19:40 +0000

    Cheked if None was passed as an argument

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2 or len(tuple_b) < 2:
-        if len(tuple_a) == 1:
-            t_a = (tuple_a[0], 0)
-            t_b = tuple_b[0:2]
+    if tuple_a is not None and tuple_b is not None:
+        if len(tuple_a) < 2 or len(tuple_b) < 2:
+            if len(tuple_a) == 1:
+                t_a = (tuple_a[0], 0)
+                t_b = tuple_b[0:2]
+            else:
+                t_a = (0, 0)
+                t_b = tuple_b[0:2]
+            if len(tuple_b) == 1:
+                    t_a = tuple_a[0:2]
+                    t_b = (tuple_b[0], 0)
+            else:
+                    t_a = tuple_a[0:2]
+                    t_b = (0, 0)
         else:
-            t_a = (0, 0)
+            t_a = tuple_a[0:2]
             t_b = tuple_b[0:2]
-        if len(tuple_b) == 1:
-            t_a = tuple_a[0:2]
-            t_b = (tuple_b[0], 0)
-        else:
-            t_a = tuple_a[0:2]
-            t_b = (0, 0)
-    else:
-        t_a = tuple_a[0:2]
-        t_b = tuple_b[0:2]
-    t_out = (t_a[0] + t_b[0], t_a[1] + t_b[1])
-    return t_out
+        t_out = (t_a[0] + t_b[0], t_a[1] + t_b[1])
+        return t_out
