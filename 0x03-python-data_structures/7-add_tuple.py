dfe54aee376f1a6b207cc5d5791f4fb43a5c6f70
JamesPagani <jaimeagalvezv@gmail.com>
2019-12-18 00:42:08 +0000

    PEP 8 style fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    t_a = tuple_a[:2] + (0,0)
-    t_b = tuple_b[:2] + (0,0)
+    t_a = tuple_a[:2] + (0, 0)
+    t_b = tuple_b[:2] + (0, 0)
     return t_a[0] + t_b[0], t_a[1] + t_b[1]
