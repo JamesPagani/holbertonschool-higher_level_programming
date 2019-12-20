098a408fde80f294fdad5d2d0bd7dc4f4bdf584c
JamesPagani <jaimeagalvezv@gmail.com>
2019-12-20 00:32:03 +0000

    Wrong directory, redoing it

--- 0x04-python_more_data_structures/0-square_matrix_simple.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    if matrix != [] and matrix is not None:
-        sqr_mtx = []
-        for i in range(len(matrix)):
-            sqr_mtx.append(list(map(lambda n: n**2, matrix[i])))
-        return sqr_mtx
