eae15b455ea9c4972e684ff3474ee54d6dd069d1
JamesPagani <jaimeagalvezv@gmail.com>
2019-12-20 00:27:16 +0000

    Fixed PEP 8 style

--- 0x04-python_more_data_structures/0-square_matrix_simple.py
+++ 0x04-python_more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix != [] and matrix is not None:
-        sqr_pow = lambda n : n ** 2
         sqr_mtx = []
         for i in range(len(matrix)):
-            sqr_mtx.append(list(map(sqr_pow, matrix[i])))
+            sqr_mtx.append(list(map(lambda n: n**2, matrix[i])))
         return sqr_mtx
