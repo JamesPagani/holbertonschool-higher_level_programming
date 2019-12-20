adc8af0f9b3d744bcd01b132598af24454d72415
JamesPagani <jaimeagalvezv@gmail.com>
2019-12-20 05:25:35 +0000

    PEP 8 Style correction

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -8,7 +8,7 @@ def best_score(a_dictionary):
         else:
             h_key = None
             for k in a_dictionary:
-                if h_key == None:
+                if h_key is None:
                     h_key = k
                 else:
                     if a_dictionary[k] > a_dictionary[h_key]:
