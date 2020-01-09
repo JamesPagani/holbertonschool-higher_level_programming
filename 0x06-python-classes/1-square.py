145f1e2dce9acbce43abd72658c6bb93a93f90c4
JamesPagani <jaimeagalvezv@gmail.com>
2020-01-09 19:29:35 +0000

    PEP8 style fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 class Square:
     """Defines a Square with a size attribute"""
-        
     def __init__(self, size):
             """Initializes the class"""
-            
             self.__size = size
