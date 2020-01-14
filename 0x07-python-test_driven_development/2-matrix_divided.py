0987934ca954c27ae726f1d35537af808f238823
JamesPagani <jaimeagalvezv@gmail.com>
2020-01-14 06:08:54 +0000

    Fixed multi-line string format for PEP8

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,19 +5,20 @@ Divide every element of a given matrix by a specified number. The matrix must
 be a list of lists.
 """
 
+
 def matrix_divided(matrix, div):
     """Check if the matrix is valid and then divides each element by div"""
     if isinstance(matrix, list) is False:
-        raise TypeError("matrix must be a matrix (list of lists) of integers" \
-        "/floats")
+        raise TypeError("""matrix must be a matrix (list of lists) of \
+integers/floats""")
     for row in matrix:
         if isinstance(row, list) is False:
-            raise TypeError("matrix must be a matrix (list of lists) of " \
-            "integers/floats")
+            raise TypeError("""matrix must be a matrix (list of lists) of \
+integers/floats""")
         for i in row:
             if isinstance(i, (int, float)) is False:
-                raise TypeError("matrix must be a matrix (list of lists) of " \
-                "integers/floats")
+                raise TypeError("""matrix must be a matrix (list of lists) of \
+integers/floats""")
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
     if isinstance(div, (int, float)) is False:
