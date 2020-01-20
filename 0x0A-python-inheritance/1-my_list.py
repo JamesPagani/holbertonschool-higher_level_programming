07feee31f23d77a5de4216ae0240ff202dbae637
JamesPagani <jaimeagalvezv@gmail.com>
2020-01-20 21:11:55 +0000

    Prints a sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""My list
+Write a class MyList that inherits from list:
+    METHODS:
+        + print_sorted(self) - Prints the list, but sorted (does not modify
+          original list)
+
+Only tested with lists filled with integers.
+"""
+
+
+class MyList(list):
+    """A class derived from the list built-in class"""
+    def print_sorted(self):
+        """Print a sorted list. Does not modify the original."""
+        print(sorted(self))
