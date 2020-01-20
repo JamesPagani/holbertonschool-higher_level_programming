cd51d36028d14eb0e81ad679dc5595a4760979bf
JamesPagani <jaimeagalvezv@gmail.com>
2020-01-20 20:04:05 +0000

    Print all atributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Lookup
+Write a function that returns the list of all available attributes and methods
+of an object.
+
+Returns a list object.
+"""
+
+
+def lookup(obj):
+    """Return the contents of an object."""
+    return dir(obj)
