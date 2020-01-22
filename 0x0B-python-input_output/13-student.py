f5cf3833118973eb3808ce663b71db04602800ec
JamesPagani <jaimeagalvezv@gmail.com>
2020-01-22 04:44:07 +0000

    Class Student that can update its instance values via JSON dictionary

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Student to JSON.
+A Student class with the folowing attributes:
+    FIELDS:
+        + first_name (Instance, Public)
+        + last_name (Instance, Public)
+        + age (Instance, Public)
+    METHODS:
+        + __init__(self, first_name, last_name, age): Init method
+        + to_json(self, attrs=None): Retrieves a dictionary description of a
+          Student instance.
+            - If attrs is a list of strings, the dictionary will contain only
+              the fields listed in in attrs.
+            - Otherwise, the dictionary will contain all fields.
+        + reload_from_json(self, json): Replaces the values of all fields by
+          the values in json (which is a dictionary)
+
+Can update its values via JSON string/a new dictionary.
+"""
+
+
+class Student:
+    """A Student at Holberton School."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new instance of Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve this instance's dictionary description."""
+        if not type(attrs) is list:
+            return self.__dict__
+        for a in attrs:
+            if not type(a) is str:
+                return self.__dict__
+
+        out_dict = {}
+        for a in attrs:
+            out_dict[a] = self.__dict__.get(a)
+        return out_dict
+
+    def reload_from_json(self, json):
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
