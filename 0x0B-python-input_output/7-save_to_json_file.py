6d71208c91c8ad12fe2ac0931d2e0458262f82d0
JamesPagani <jaimeagalvezv@gmail.com>
2020-01-22 01:19:26 +0000

    Changed file mode to Write

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -9,5 +9,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Write to a file a JSON string of my_object."""
-    with open(filename, mode="a") as json_file:
+    with open(filename, mode="w") as json_file:
         json_file.write(json.dumps(my_obj))
