58ac4a40dc5254db039ec613d5664df0ca806b2d
JamesPagani <jaimeagalvezv@gmail.com>
2020-01-27 23:06:27 +0000

    Square class that inherits from Rectangle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """A basic square.."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, \
+                                                 self.width)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        self.width = size
+        self.height = size
+
+    def update(self, *args, **kwargs):
+        if len(args) > 0:
+            try:
+                self.id = args[0]
+                self.width = args[1]
+                self.height = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            except:
+                pass
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.width = kwargs["size"]
+                self.height = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
