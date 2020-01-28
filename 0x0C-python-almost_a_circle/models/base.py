7cbd72d87deab4245559acdf9a1838ec966ea728
JamesPagani <jaimeagalvezv@gmail.com>
2020-01-28 04:47:52 +0000

    Converts a list of dictionaries into a JSON representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,9 @@ of each instance ID.
 """
 
 
+import json
+
+
 class Base:
     """Parent class. Keeps track of each class instance's ID."""
 
@@ -17,3 +20,10 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Convert list_dictionaries into a JSON string representation."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
