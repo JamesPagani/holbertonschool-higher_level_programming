51be3f9334251ab2325283e75cef30980da28942
JamesPagani <jaimeagalvezv@gmail.com>
2020-01-28 03:25:24 +0000

    Added docstrings to the methods

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Square class.
+A rectangle whose width and height are the same size, also known as a square.
+"""
+
 
 from models.rectangle import Rectangle
 
@@ -7,22 +11,27 @@ class Square(Rectangle):
     """A basic square.."""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square."""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """Informal string representation of a square."""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.width)
 
     @property
     def size(self):
+        """Size (width and height) getter."""
         return self.width
 
     @size.setter
     def size(self, size):
+        """Size (width and height) setter."""
         self.width = size
         self.height = size
 
     def update(self, *args, **kwargs):
+        """Update the id, size, x and y offsets of a square."""
         if len(args) > 0:
             try:
                 self.id = args[0]
