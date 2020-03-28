91cd76979a9fcda2f9cb6e6bdfa14d78ae59a9ff
JamesPagani <jaimeagalvezv@gmail.com>
2020-03-28 20:25:00 +0000

    Just in case, specified host and port

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,8 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    hbtn_db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    hbtn_db = MySQLdb.connect(localhost="localhost", user=argv[1],
+                              passwd=argv[2], db=argv[3], port=3306)
     hbtn_db.query("""SELECT * FROM states ORDER BY id ASC;""")
     table = dbtn_db.store_result()
 
