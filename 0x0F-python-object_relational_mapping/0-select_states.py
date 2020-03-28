12e14be2e5fb779b124f99dd6d586c0dc9893cf8
JamesPagani <jaimeagalvezv@gmail.com>
2020-03-28 20:32:39 +0000

    Fixed typos, again (localhost -> host as a kwarg; dbtn_db -> hbtn_db)

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,10 +7,10 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    hbtn_db = MySQLdb.connect(localhost="localhost", user=argv[1],
+    hbtn_db = MySQLdb.connect(host="localhost", user=argv[1],
                               passwd=argv[2], db=argv[3], port=3306)
     hbtn_db.query("""SELECT * FROM states ORDER BY id ASC;""")
-    table = dbtn_db.store_result()
+    table = hbtn_db.store_result()
 
     for i in table.fetch_row(maxrows=0):
         print(i)
