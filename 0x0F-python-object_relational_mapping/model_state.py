d3554c27f0ad7b9b30900fbc5987852bc43ae2f5
JamesPagani <jaimeagalvezv@gmail.com>
2020-03-30 06:38:56 +0000

    Class interpretation of a MySQL table

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Model State.
+Sets the model of a MySQL table ('state').
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+class State(Base):
+    """SAMPLE TEXT."""
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
