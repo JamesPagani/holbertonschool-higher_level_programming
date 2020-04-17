c6d3831d8f2ddb58453aabe93ad2ba3469be5218
JamesPagani <jaimeagalvezv@gmail.com>
2020-04-17 06:19:56 +0000

    Made the module not run when imported; Gave it execution permissions and added the URL to the get method

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,9 +7,9 @@ Make a request to Holberton's platform using the 'requests' library.
 
 import requests
 
+if __name__ == "__main__":
+    response =  requests.get("https://intranet.hbtn.io/status")
 
-response =  requests.get("")
-
-print("Body response:")
-print("\t- type: {}".format(type(response.text)))
-print("\t- content: {}".format(response.text))
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
