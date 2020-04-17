bee2fc27e367cfd1614246107391b7636dacc64f
JamesPagani <jaimeagalvezv@gmail.com>
2020-04-17 06:53:13 +0000

    PEP8 Fixes: Closed gap between keyworded argument and value; Added a whitespace after the dictionary key (in data); Erased extra whitespace

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,7 +11,6 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    response = requests.post(argv[1], data = {"email":argv[2]})
+    response = requests.post(argv[1], data={"email": argv[2]})
 
     print(response.text)
-
