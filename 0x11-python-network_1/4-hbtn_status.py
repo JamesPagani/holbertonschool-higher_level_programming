cb24b1fbc221f639531d3b1ebddeddbd53ec1784
JamesPagani <jaimeagalvezv@gmail.com>
2020-04-17 06:22:36 +0000

    PEP8 fix: Too many spaces after operator (when making the HTTP request)

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,7 +8,7 @@ Make a request to Holberton's platform using the 'requests' library.
 import requests
 
 if __name__ == "__main__":
-    response =  requests.get("https://intranet.hbtn.io/status")
+    response = requests.get("https://intranet.hbtn.io/status")
 
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
