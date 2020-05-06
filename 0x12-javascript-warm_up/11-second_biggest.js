1a8c67a74bcc4fa5d88d99d057d3787979ef076f
= <=>
2020-05-06 02:46:54 +0000

    Find the second highest number in a list.

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const nums = process.argv.slice(2);
+let highest = nums[0];
+let second = nums[1];
+
+if (nums.length === 0 || nums.length === 1) {
+  console.log(0);
+} else {
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > highest) {
+      second = highest;
+      highest = nums[i];
+    } else if (nums[i] > second && nums[i] < highest) {
+      second = nums[i];
+    }
+  }
+  console.log(second);
+}
