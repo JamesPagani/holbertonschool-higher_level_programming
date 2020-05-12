248ef3af823154d9e9dcd2901e91b73b03ac7c0a
= <=>
2020-05-12 04:30:37 +0000

    tsil a sesreveR

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  const tsil = [];
+  for (let i = 0; i < list.length; i++) {
+    tsil[i] = list[list.length - (1 + i)];
+  }
+  return tsil;
+};
