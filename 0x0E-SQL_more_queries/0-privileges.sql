85bc8530da46683bdb56b57a665e37f101314c16
= <=>
2020-06-25 07:19:37 +0000

    Reverting previous change

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- Shows the privileges for the users 'user_0d_1' and 'user_0d_2' in localhost
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Keep in mind that an error will show if an user does not have privileges
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
