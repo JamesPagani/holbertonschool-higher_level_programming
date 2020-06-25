83041bb7b1262d87fa157b6fef215244646c2eaa
= <=>
2020-06-25 08:28:12 +0000

    Only focuses on searching through the API, should the host change domain name

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const target = 'https://swapi-api.hbtn.io/api/people/18/';
+const target = '/api/people/18/';
 
 request.get(process.argv[2], function (error, response, body) {
   if (error) {
@@ -9,8 +9,12 @@ request.get(process.argv[2], function (error, response, body) {
     const films = JSON.parse(body).results;
     let appears = 0;
     for (let i = 0; i < JSON.parse(body).count; i++) {
-      if (films[i].characters.includes(target)) {
-        appears++;
+      for (let j = 0; j < films[i].characters.length; j++){
+        if (films[i].characters[j].includes(target))
+        {
+          appears++;
+          break;
+        }
       }
     }
     console.log(appears);
